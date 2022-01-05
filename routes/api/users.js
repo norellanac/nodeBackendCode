b15db343bb0a5627a506807a89879fb7f4d51b0b
norellanac <1005alexis@gmail.com>
2022-01-05 16:23:08 -0600

    user registration and validating if user exists

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,43 @@
 const express = require("express");
+const User = require("../../models/User");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
+//@route    GET api/profile/test
+//@des  Tests profile route
+//@access   Public
 router.get("/test", (req, res) => res.json({ msg: "Routes Works" }));
 
+//@route    GET api/profile/test
+//@des  Tests profile route
+//@access   Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: "Email Already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+      const newUSer = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password,
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUSer.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUSer.password = hash;
+          newUSer.save()
+          .then(user => res.json(user))
+          .catch(err => res.json(err))
+        });
+      });
+    }
+  });
+});
 module.exports = router;
